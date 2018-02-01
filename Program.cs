b35a293e8f7fa810028bbfd5e84a267808d97ed6
ehron <ehron@teamgwinn.com>
2018-01-31 20:45:24 -0500

    move stuff

--- Program.cs
+++ Program.cs
@@ -15,18 +15,11 @@ namespace RewrapExample
         }
 
         static async Task RunAsync() {
-            string baseUrl = "https://randomuser.me";
-            WebHelper.client.BaseAddress = new Uri(baseUrl);
-            WebHelper.client.DefaultRequestHeaders.Accept.Clear();
-            WebHelper.client.DefaultRequestHeaders.Accept.Add(
-                new MediaTypeWithQualityHeaderValue("application/json")
-            );    
-            //Console.WriteLine($"Connecting to ")
-            string query = "/api/?results=10&nat=us";
+            
             // retrieve data for DB
             await DBHelper.CreateDBAsync();
             await DBHelper.CreateTablesAsync();
-            WebHelper.ApiResults apiResults = await WebHelper.GetUserRecordsAsync(baseUrl+query);
+            WebHelper.ApiResults apiResults = await WebHelper.GetUserRecordsAsync(baseUrl + query);
             Console.WriteLine(apiResults.Records);
             foreach (var record in apiResults.Records) {
                 Console.WriteLine(record.Name.First);
